4001b21e3e9e47265b05b88819b1881a4891b58c
John Mark Redding <johnmarkredding@gmail.com>
2018-11-09 22:25:47 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,23 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index {|val, idx|
+    puts "#{idx+1}. #{val}"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls.collect {|x|
+    x = x.capitalize + "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  result = calls.select {|val|
+    val.size > 4
+  }
+  !(result.empty?)
 end
 
 def find_the_cheese# code an argument here
